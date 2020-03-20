dae1da192ddc699757e1b6f9ee1f086cdf5d0607
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2020-03-20 14:53:41 -0400

    fixed bug in figure when no middle term is found

--- tests/Figure.test.js
+++ tests/Figure.test.js
@@ -1,18 +1,13 @@
 const Figure = require('../lib/Figure');
 
 
-it('Term has label created from integer', () => {
-  const f = new Figure(1)
-  const result = f.label
-  expect(result).toBe("1")
-});
-it('Term has label when created from string', () => {
+it('figure has label ', () => {
   const f = new Figure("1")
   const result = f.label
   expect(result).toBe("1")
 });
-it('Term has label when created from integer return integer', () => {
-  const f = new Figure(1)
-  const result = f.number
-  expect(result).toBe(1)
+it('figure has label ', () => {
+  const f = new Figure("unknown")
+  const result = f.label
+  expect(result).toBe("unknown")
 });
--- tests/Form.test.js
+++ tests/Form.test.js
@@ -4,7 +4,7 @@ const Figure = require('../lib/Figure');
 
 
 it('Term has label created from integer', () => {
-  const form = new Form(new Mood("A", "A", "A"), new Figure(1))
+  const form = new Form(new Mood("A", "A", "A"), new Figure("1"))
   const result = form.label
   expect(result).toBe("AAA1")
 });
